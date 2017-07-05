4951c6c1f5039b5458115ac7535319cdcccb96a7
ch <807672412@qq.com>
2017-07-06 00:01:47 +0800

    更新EventUtil和CookieUtil

---
+++ myJsTools/CookieUtil.js
@@ -0,0 +1,69 @@
+;(function(win, doc) {
+
+    /**
+     * @type {{get: 获取cookie, set: 设置cookie, unset: 删除cookie}}
+     * @author slim。
+     * @date 2017.07.05
+     */
+    var CookieUtil = {
+        get: function(name) {
+
+            //设置需要查找的cookie名字
+            var cookieName = encodeURIComponent(name) + "=";
+
+            //根据cookieName查找
+            var cookieStart = doc.cookie.indexOf(cookieName);
+            var cookieValue = null;
+
+            //判断是否查找到了cookieName
+            if(cookieStart > -1) {
+
+                //从cookieStatr位置开始查找第一个分号
+                var cookieEnd = doc.cookie.indexOf(";", cookieStart);
+
+                //如果不存在，则表示后面的都是name
+                if(cookieEnd == -1) {
+                    cookieEnd = doc.cookie.length;
+                }
+
+                //使用substring来截取字符串并转码赋值给cookieValue
+                cookieValue = encodeURIComponent(doc.cookie.substring(cookieStart + cookieName.length, cookieEnd));
+            }
+            return cookieValue;
+        },
+        set: function(name, value, expires, path, domain, secure) {
+            var cookieText = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+            //是否存在expires
+            expires instanceof Date && (cookieText += "; expires=" + expires.toGMTString());
+
+            //是否存在path
+            path && (cookieText += "; path=" + path);
+
+            //是否存在domain
+            domain && (cookieText += "; domain=" + domain);
+
+            //是否存在secure
+            secure && (cookieText += "; secure");
+
+            doc.cookie = cookieText;
+        },
+        unset: function(name, path, domain, secure) {
+
+            //通过设置过期时间达到删除目的
+            this.set(name, path, new Date(0), domain, secure);
+        }
+    }
+
+    //检测是否为AMD、CMD或window
+    if(typeof exports == "object") {
+        module.exports = CookieUtil;
+    } else if(typeof define == "function" && define.amd) {
+        define([], function() {
+            return CookieUtil;
+        });
+    } else {
+        win.CookieUtil = CookieUtil;
+    }
+
+})(window, document, undefined);
---
+++ myJsTools/EventUtil.js
@@ -0,0 +1,60 @@
+;(function(win, doc) {
+    var EventUtil = {
+
+        //获取事件对象
+        getEvent: function(event) {
+            return event ? event : window.event;
+        },
+        //获取事件对象目标
+        getTarget: function(event) {
+            return event.target || event.srcElement;
+        },
+        //阻止默认事件
+        preventDefault: function(event) {
+            if(event.preventDefault) {
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+        },
+        //阻止冒泡事件
+        stopPropagation: function(event) {
+            if(event.stopPropagation) {
+                event.stopPropagation();
+            } else {
+                event.cancelBubble = true;
+            }
+        },
+        //添加事件
+        addEvent: function(ele, type, fn) {
+            if(ele.addEventListener) {
+                ele.addEventListener(type, fn, false);
+            } else if(ele.attchEvent) {
+                ele.attachEvent("on" + type, fn);
+            } else {
+                ele["on" + type] = fn;
+            }
+        },
+        //移除事件
+        removeEvent: function(ele, type, fn) {
+            if (ele.removeEventListener) {
+                ele.removeEventListener(type, fn, false);
+            } else if (ele.detachEvent) {
+                ele.detachEvent("on" + type, fn);
+            } else {
+                ele["on" + type] = null;
+            }
+        }
+    }
+
+    //兼容amd、cmd和window
+    if(typeof exports == "object") {
+        module.exports = EventUtil;
+    } else if(typeof define == "function" && define.amd) {
+        define([], function() {
+            return EventUtil;
+        });
+    } else {
+        win.EventUtil = EventUtil;
+    }
+})(window, document, undefined);
